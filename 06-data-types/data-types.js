13cc62503ad1f896a83cb6592b955378f6b09038
Hellpython <intimate4869@naver.com>
2026-06-15 11:30:13 +0900

    feat: 6장 예제 추가

---
+++ 06-data-types/data-types.js
@@ -0,0 +1,4 @@
+console.log(typeof null);        // 예측: ?  (유명한 함정)
+console.log(typeof NaN);         // 예측: ?
+console.log(0.1 + 0.2);          // 예측: 0.3 맞을까?
+console.log(0.1 + 0.2 === 0.3);  // 예측: ?
